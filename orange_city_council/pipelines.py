1a12a3f69ba2865513814d93cdc1ace0d4427e94
Matthew Landauer <mlandauer@gmail.com>
2019-05-07 14:04:05 +1000

    Always save record as planningalerts now does versioning

--- orange_city_council/pipelines.py
+++ orange_city_council/pipelines.py
@@ -11,24 +11,6 @@ import scraperwiki
 
 class OrangeCityCouncilPipeline(object):
     def process_item(self, item, spider):
-        try:
-            existing = scraperwiki.sql.select(
-                "* FROM data WHERE council_reference=?",
-                [item['council_reference']]
-            )
-        except:
-            # First run?  No file / table yet.
-            existing = False
-
-        if existing:
-            spider.log(
-                "Skipping previously saved application: {}".format(
-                    item['council_reference']
-                ),
-                level=INFO
-            )
-            return
-
         unique_keys = ['council_reference']
         scraperwiki.sql.save(unique_keys, dict(item), table_name='data')
         spider.log("Saved {}".format(item['council_reference']), level=INFO)
